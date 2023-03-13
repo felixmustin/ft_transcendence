9844e742af6f041ebccdfdbafb6b61f42fe93190
NetworkFlux <75378448+NetworkFlux@users.noreply.github.com>
2023-03-13 11:56:36 +0100

    login/signup css

--- frontend/src/components/npinheir/Npinheir.tsx
+++ frontend/src/components/npinheir/Npinheir.tsx
@@ -6,7 +6,7 @@ type Props = {}
 const Npinheir = (props: Props) => {
 
   return (
-    <div className="app bg-gradient-to-b from-violet-900 via-violet-500 to-blue-800">
+    <div className="app bg-gradient-to-tl from-violet-900 via-violet-900 to-black">
       <Navbar />
     </div>
   )
