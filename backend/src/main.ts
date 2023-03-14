fafe046ef0d07978e3e27e267bb56953a42a7154
felixmustin <60741148+felixmustin@users.noreply.github.com>
2023-03-14 13:09:42 +0100

    42auth + cookies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({ origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'ws://127.0.0.1:3000', 'ws://localhost:3000'] });
-  // app.use(cors({ origin: 'http://127.0.0.1:3000' }));
+  app.use(cors({ origin: 'http://localhost:3000' }));
   await app.listen(3001);
   console.log(`Running on port 3001.`);
 }
